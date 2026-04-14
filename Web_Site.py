b62347b79c33004c64e320eb8c1d0d54d53a7582
jgautomation <gurjarjitendra25@gmail.com>
2026-04-14 16:40:53 +0530

    Add files via upload

---
+++ Web_Site.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, jsonify
+import random
+
+app = Flask(__name__)
+
+# Home Page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Services Page
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+# Contact Page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# Dashboard Page
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+# Dummy Data API
+@app.route('/data')
+def data():
+    return jsonify({
+        "temperature": random.randint(20, 80),
+        "pressure": random.randint(1, 10),
+        "production": random.randint(100, 500),
+        "status": random.choice(["ON", "OFF"])
+    })
+
+if __name__ == '__main__':
+    app.run()
